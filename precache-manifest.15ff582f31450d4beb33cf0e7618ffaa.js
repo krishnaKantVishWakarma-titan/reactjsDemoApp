bd396b5136e36e4f0f7ac3137710fef111274f7b
krishnaKantVishWakarma-titan <63912483+krishnaKantVishWakarma-titan@users.noreply.github.com>
2021-01-09 18:47:45 +0530

    Add files via upload

---
+++ precache-manifest.15ff582f31450d4beb33cf0e7618ffaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2368d300995a475da79f7115f4d2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25df8bdb910bec1cf727",
+    "url": "/static/js/2.67d685dd.chunk.js"
+  },
+  {
+    "revision": "a1212b395e88f070a42368ea2ff93e06",
+    "url": "/static/js/2.67d685dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80410e3e4c59aea4e254",
+    "url": "/static/js/main.664a0e71.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
